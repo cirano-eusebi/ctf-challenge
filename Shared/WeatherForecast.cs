4ad1de5187474b7da24a57d76e0a56e5e22d921e
Cirano Eusebi <eusebi.cirano@gmail.com>
2022-09-26 11:20:13 -0300

    Add scaffolded wasm hosted solution

---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CFTChallenge.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
