bb794de646747d4dd1f9fda62eb9d8bcf9afba54
Cirano Eusebi <eusebi.cirano@gmail.com>
2022-10-02 16:08:22 -0300

    Add Restart button

---
+++ Server/Controllers/RestartController.cs
@@ -0,0 +1,23 @@
+﻿// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTFChallenge.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RestartController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Restart()
+        {
+            // Fire and forget a delayed stop of the application
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                Environment.Exit(0);
+            });
+            return Ok();
+        }
+    }
+}
